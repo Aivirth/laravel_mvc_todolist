6388644d9fecd53141af2a27814d5c78775359ae
Aivirth <aivirth.dev@gmail.com>
2019-05-19 18:01:34 +0200

    Added missing user_id into ProjectsSeeder

--- database/seeds/ProjectsSeeder.php
+++ database/seeds/ProjectsSeeder.php
@@ -14,21 +14,25 @@ class ProjectsSeeder extends Seeder
     DB::table('projects')->insert([
       'title' => 'Project 1',
       'description' => Str::random(5) . ' ' . Str::random(10) . ' ' . Str::random(10),
+      'user_id' => 1
     ]);
 
     DB::table('projects')->insert([
       'title' => 'Project 2',
       'description' => Str::random(5) . ' ' . Str::random(8) . ' ' . Str::random(15),
+      'user_id' => 2
     ]);
 
     DB::table('projects')->insert([
       'title' => 'Project 3',
       'description' => Str::random(8) . ' ' . Str::random(15) . ' ' . Str::random(6),
+      'user_id' => 2
     ]);
 
     DB::table('projects')->insert([
       'title' => 'Project 4',
       'description' => Str::random(5) . ' ' . Str::random(8) . ' ' . Str::random(15),
+      'user_id' => 1
     ]);
   }
 }

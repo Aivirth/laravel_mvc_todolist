718147853037f98cb0098b8109d3c4cb7a3c2061
Aivirth <aivirth.dev@gmail.com>
2019-05-19 17:16:10 +0200

    Added projects table schema

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+  protected $fillable = ['title', 'project_id', 'description'];
 }
--- database/migrations/2019_05_19_150924_create_projects_table.php
+++ database/migrations/2019_05_19_150924_create_projects_table.php
@@ -6,26 +6,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProjectsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('projects', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->text('title');
+      $table->string('description', 100);
+      $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+      $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('projects');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('projects');
+  }
 }

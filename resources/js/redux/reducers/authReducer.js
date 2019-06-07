fa3f9a9d1080343a3275e3022c92fde06d7e228b
Aivirth <aivirth.dev@gmail.com>
2019-06-07 09:09:02 +0200

    Added date-fns conversion to localStorage token

--- resources/js/redux/reducers/authReducer.js
+++ resources/js/redux/reducers/authReducer.js
@@ -1,15 +1,17 @@
 import * as actionTypes from "../actions/actionsTypes";
 import { updateObject } from "../../helpers";
+import { addMinutes, format as formatDate } from "date-fns";
 
 const initialState = {
     errors: null,
     user: null,
     access_token: null,
     token_type: null,
-    expires_in: null
+    expires: null
 };
 
 const loginError = (state, action) => {
+    console.log(action);
     return updateObject(state, {
         errors: action.error.response
     });
@@ -18,14 +20,22 @@ const loginError = (state, action) => {
 const loginSuccess = (state, action) => {
     const { user, access_token, token_type, expires_in } = action.data;
 
-    localStorage.setItem("laravelMVCToken", access_token);
+    const expirationDate = addMinutes(new Date(), expires_in);
+
+    localStorage.setItem(
+        "laravelMVC",
+        JSON.stringify({
+            token: access_token,
+            expires: expirationDate
+        })
+    );
 
     return updateObject(state, {
         errors: null,
         user: user,
         access_token: access_token,
         token_type: token_type,
-        expires_in: expires_in
+        expires: expirationDate
     });
 };
 

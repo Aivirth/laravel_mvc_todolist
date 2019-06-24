8b667272b3c9fe7c2f366cefb542251f6dd533f2
Aivirth <aivirth.dev@gmail.com>
2019-06-24 22:08:38 +0200

    Added cron schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,39 +4,46 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan as Artisan;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    //
+  ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    // $schedule->command('inspire')
+    //          ->hourly();
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    $schedule->call(function () {
+      // Artisan::call('migrate', ['--seed' => true]);
+      Artisan::call('migrate:refresh');
+      Artisan::call('db:seed');
+    })->daily();
+  }
 
-        require base_path('routes/console.php');
-    }
+  /**
+   * Register the commands for the application.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    $this->load(__DIR__ . '/Commands');
+
+    require base_path('routes/console.php');
+  }
 }

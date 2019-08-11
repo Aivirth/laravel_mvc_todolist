87fa6379605fa1b52620059651c358e7835e0c34
Aivirth <aivirth.dev@gmail.com>
2019-08-11 05:22:55 +0200

    Changed axios base url to use env variables

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("laravelMVCtoken") || null;
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: `${process.env.MIX_APP_URL}/api/`,
     timeout: 5000
 });
 
